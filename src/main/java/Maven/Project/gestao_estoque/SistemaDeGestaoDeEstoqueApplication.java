98cc2704fa7aeb261df2689f6a52d25bf97fcbcc
Filipe Augusto <filipeaugusto1@live.com>
2024-09-02 19:52:16 -0300

    Primeiro commit - Inicializando o projeto de Gestão de Estoque

---
+++ src/main/java/Maven/Project/gestao_estoque/SistemaDeGestaoDeEstoqueApplication.java
@@ -0,0 +1,13 @@
+package Maven.Project.gestao_estoque;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaDeGestaoDeEstoqueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaDeGestaoDeEstoqueApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Maven/Project/gestao_estoque/SistemaDeGestaoDeEstoqueApplicationTests.java
@@ -0,0 +1,13 @@
+package Maven.Project.gestao_estoque;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaDeGestaoDeEstoqueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
